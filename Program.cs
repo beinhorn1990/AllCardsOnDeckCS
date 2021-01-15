dcc2d01ce06959404aee56880a2d098c9a84c17a
Brendan Einhorn <beinhorn90@gmail.com>
2021-01-14 20:20:34 -0500

    create deck

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AllCardsOnDeckCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            var suits = new List<string>() { "Hearts", "Diamonds", "Clubs", "Spades" };
+            var ranks = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+            var deck = new List<string>();
+            foreach (var suit in suits)
+            {
+                foreach (var rank in ranks)
+                {
+                    deck.Add($"{rank} of {suit}");
+                }
+
+            }
+        }
     }
-  }
 }
